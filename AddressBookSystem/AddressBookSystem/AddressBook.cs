f153605cb00bc01785e3a0f9712637ee42af0ccc
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-19 19:06:16 +0530

    Create Contact Details

--- AddressBookSystem/AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -9,5 +9,26 @@ namespace AddressBookSystem
     // create class in this Defining Method for Address Book System..
     internal class AddressBook
     {
+        // creating String & Int Type  Variable to Store Details
+        string First_Name;
+        string Last_Name;
+        string Address;
+        string City;
+        string State;
+        int Zip_Code;
+        long Phone_Number;
+        string Email_Address;
+
+        // Defining Paramterized Method/Function or instance Method
+        public void ContactDetails(string First_Name, string Last_Name,String Address,string City, string State,int Zip_Code,long Phone_number,string Email_Address)
+        {
+            // printg Details..
+            Console.WriteLine("\nContact Details are :-");
+            Console.WriteLine("Name : {0} {1}", First_Name, Last_Name);
+            Console.WriteLine("Address : {0}, {1} ({2})",Address,City,State);
+            Console.WriteLine("Zip Code : {0}", Zip_Code);
+            Console.WriteLine("Ph. Number : {0}", Phone_number);
+            Console.WriteLine("Email Address : {0}", Email_Address);
+        }
     }
 }
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -10,6 +10,12 @@ namespace AddressBookSystem
             // printing Welcome Message
             Console.WriteLine("\t\t\t******* WELCOME TO ADDRESS BOOK SYSTEM PROGRAM ******");
 
+            // creating Instance/Object of Class AddressBook
+            AddressBook objdetails = new AddressBook();
+
+            // Calling Method & Giving Parameters
+            objdetails.ContactDetails("Ravi", "Ranjan", "DLF Phase 4", "Gurugram", "Haryana", 122002, 9319964621, "Ezekie.em@gmail.com");
+
             // For Holding Output Terminal
             Console.ReadKey();
         }
