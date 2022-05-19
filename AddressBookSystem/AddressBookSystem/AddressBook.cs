3f8ab3229b380f10ebf07ebbc82c3089d2ebf505
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-19 17:52:48 +0530

    Welcome Message

---
+++ AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    // create class in this Defining Method for Address Book System..
+    internal class AddressBook
+    {
+    }
+}
---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    public class Program
+    {
+        // Create Static Main Method 
+        static void Main(string[] args)
+        {
+            // printing Welcome Message
+            Console.WriteLine("\t\t\t******* WELCOME TO ADDRESS BOOK SYSTEM PROGRAM ******");
+
+            // For Holding Output Terminal
+            Console.ReadKey();
+        }
+    }
+}
