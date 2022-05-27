ac3b977b50fbdc672ded6fe1701d175ca64c09eb
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-27 14:02:53 +0530

    Print Contact Details

--- AddressBookSystem/AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -9,8 +9,44 @@ namespace AddressBookSystem
     // create class for Defining Method/Function for Address Book System..
     internal class AddressBook
     {
-       
-        
+        // creating String & Int Type  Variable to Store Details
+        public string First_Name;
+        public string Last_Name;
+        public string Address;
+        public string City;
+        public string State;
+        public int Zip_Code;
+        public long Phone_Number;
+        public string Email_Address;
 
+
+        // Defining  Constructor
+        public AddressBook(string First_Name, string Last_Name, String Address, string City, string State, int Zip_Code, long Phone_Number, string Email_Address)
+        {
+
+            // set Class level Variables Value from the Constructor
+            this.First_Name = First_Name;
+            this.Last_Name = Last_Name;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.Zip_Code = Zip_Code;
+            this.Phone_Number = Phone_Number;
+            this.Email_Address = Email_Address;
+
+        }
+        // creating Method/Function to Displaying Details..
+        public void DisplayContactDetails()
+        {
+            // printg Details..
+            Console.WriteLine("\nContact Details are :-");
+            Console.WriteLine("Name : {0} {1}", First_Name, Last_Name);
+            Console.WriteLine("Address : {0}, {1} ({2})", Address, City, State);
+            Console.WriteLine("Zip Code : {0}", Zip_Code);
+            Console.WriteLine("Ph. Number : {0}", Phone_Number);
+            Console.WriteLine("Email Address : {0}", Email_Address);
+
+
+        }
     }
 }
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -10,8 +10,13 @@ namespace AddressBookSystem
             // printing Welcome Message
             Console.WriteLine("\t\t\t******* WELCOME TO ADDRESS BOOK SYSTEM PROGRAM ******");
 
-           
-            
+            // creating Instance/Object of Class AddressBook & setting Parameters value..
+            AddressBook objdetails = new AddressBook("Ravi", "Ranjan", "DLF Phase 4", "Gurugram", "Haryana", 122002, 9319964621, "Ezekie.em@gmail.com");
+
+            // Calling Method Define in Address Class to Displaying Details.
+            objdetails.DisplayContactDetails();
+
+
 
             // To Hold Output Terminal
             Console.ReadKey();
