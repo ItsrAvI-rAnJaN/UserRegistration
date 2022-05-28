0743cb2276026d9ac6fd12b92fe5219fe52febc8
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-28 11:19:30 +0530

    Delete Contact Details

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -10,16 +10,89 @@ namespace AddressBookSystem
             // printing Welcome Message
             Console.WriteLine("\t\t\t******* WELCOME TO ADDRESS BOOK SYSTEM PROGRAM ******");
 
-            // creating Instance/Object of Class AddressBook & setting Parameters value..
-            AddressBook objdetails = new AddressBook("Ravi", "Ranjan", "DLF Phase 4", "Gurugram", "Haryana", 122002, 9319964621, "Ezekie.em@gmail.com");
+            int Choice;
+            do
+            {
+                // taking output from user to Executed the Program
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("1.Get Contact Present in Address Book ");
+                Console.WriteLine("2.Add New Contact to Address Book");
+                Console.WriteLine("3.Edit Contact Present in Address Book");
+                Console.WriteLine("4.Delete Contact Using Person's First Name");
+                Console.Write("Enter Your Choice : ");
+                Choice = Convert.ToInt32(Console.ReadLine());
+                switch (Choice)
+                {
+                    case 1:
+                        // creating Instance/Object of Class AddressBook & setting Parameters value..
+                        AddressBook objdetails = new AddressBook("Ravi", "Ranjan", "DLF Phase 4", "Gurugram", "Haryana", 122002, 9319964621, "Ezekie.em@gmail.com");
 
-            // Calling Method Define in Address Class to Displaying Details.
-            objdetails.DisplayContactDetails();
+                        // Calling Method Define in Address Class to Displaying Details.
+                        objdetails.DisplayContactDetails();
+                        break;
+                    case 2:
+                        ContactPerson objaddnewcontact = new ContactPerson();
+                        objaddnewcontact.AddNewContact();
+                        objaddnewcontact.DisplayContactDetails();
+                        break;
+                    case 3:
+                        // creating instance & call the Method to Add New Contact
+                        ContactPerson objeditcontact = new ContactPerson();
+                        objeditcontact.AddNewContact();
+                        objeditcontact.DisplayContactDetails();
 
+                        // for Editing Contact & call the Method of Editing Contact
+                        Console.WriteLine("Edit Contact Using first Name ?  Y/N"); // asking for Edit or Not..
+                        Console.Write("Enter Your Choice : ");
+                        string Choice_3 = Console.ReadLine();
+                        if (Choice_3 == "Y")
+                        {
+                            objeditcontact.EditContactDetails();
+                            objeditcontact.DisplayContactDetails();
+                        }
+                        break;
+                    case 4:
+                        // Creting instance of call the Method to Edit & Delete Contactg Details.
+                        ContactPerson objDeleteAddress =  new ContactPerson();
+                        objDeleteAddress .AddNewContact();
+                        objDeleteAddress.DisplayContactDetails();
 
+                        // Asking to Edit Contact if Y then Proceed.
+                        Console.WriteLine("Edit Contact Using first Name ? Y/N");
+                        Console.Write("Enter Your Choice : "); 
+                        string Choice4_1 = Console.ReadLine();
+                        if (Choice4_1 == "Y")
+                        {
+                            objDeleteAddress.EditContactDetails();
+                            objDeleteAddress.DisplayContactDetails();
+                        }
+
+                        // Asking to delete the Contact Details if Y then Proceed..
+                        Console.WriteLine(" Delete Contact Using First Name ? Y/N");
+                        Console.Write("Enter Your Choice : ");
+                        string Choice4_2 = Console.ReadLine();
+                        if (Choice4_2 == "Y")
+                        {
+                            objDeleteAddress.DeleteContactDetails();
+                        }
+                        else
+                        {
+                            objDeleteAddress.DisplayContactDetails();
+                        }
+                        break;
+                            
+                    default:
+                        Console.WriteLine("Enter Correct Choice");
+                        break;
+                }
+
+            }
+            while (Choice != 0);
 
             // To Hold Output Terminal
             Console.ReadKey();
+
+
         }
     }
 }
