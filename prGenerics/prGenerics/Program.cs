65faec91d35133a7a894bab6f7e2ca25c68ef320
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-29 13:42:30 +0530

    Welcome Msg for Generics

---
+++ prGenerics/prGenerics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace prGenric
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t************ WELCOME TO GENERICS PRACTICE PROBLEM ***************");
+            Console.ReadKey();
+        }
+    }
+}
