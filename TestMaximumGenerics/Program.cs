12e22d9d094749b3e2d83a5ec99db5b8e56c8659
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-30 22:10:36 +0530

    welcome msg Genrics Problem

---
+++ TestMaximumGenerics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestMaximumGenerics
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t******** WELCOME TO FIND MAXIMUM PROBLEM USING GENERICS ********");
+            Console.ReadKey();
+        }
+    }
+}
