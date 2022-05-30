d6ddb8e1a681e63a5bb3f94336c20dcfb1cb6916
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-30 23:54:49 +0530

    Find Max Int Among Three

--- TestMaximumGenerics/Program.cs
+++ TestMaximumGenerics/Program.cs
@@ -7,6 +7,30 @@ namespace TestMaximumGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t******** WELCOME TO FIND MAXIMUM PROBLEM USING GENERICS ********");
+
+            int Choice;
+            do
+            {
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("1.Find Maximum Among Three Integer");
+                Console.Write("Enter Your Choice : ");
+                Choice=Convert.ToInt32(Console.ReadLine());
+
+                switch (Choice)
+                {
+                    case 1:
+                        FindMaxInt objFindMaxInt =new FindMaxInt();
+
+                        Console.WriteLine("Maximum Integer Number = {0}", objFindMaxInt.FindMaxAmongInt(30, 20, 10));
+
+                        Console.WriteLine("Maximum Integer Number = {0}", objFindMaxInt.FindMaxAmongInt(10, 30, 20));
+
+                        Console.WriteLine("Maximum Integer Number = {0}", objFindMaxInt.FindMaxAmongInt(10, 20, 30));
+
+                        break;
+                }
+            } while(Choice !=0);
+
             Console.ReadKey();
         }
     }
