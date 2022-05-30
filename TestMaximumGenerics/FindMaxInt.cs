a7ebbc446fffba31cfbfccb31441cf6825c2e2cd
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-31 00:09:59 +0530

    Find Max Int Number

--- TestMaximumGenerics/FindMaxInt.cs
+++ TestMaximumGenerics/FindMaxInt.cs
@@ -8,11 +8,13 @@ namespace TestMaximumGenerics
 {
     internal class FindMaxInt
     {
+        // Method to Find Max Integer Number
         public int FindMaxAmongInt(int Num1, int Num2, int Num3)
         {
+            // Use CompareTO Comparision B/W Three Integer Number
             if (Num1.CompareTo(Num2) > 0 && Num1.CompareTo(Num3) > 0)
             {
-                return Num1;
+                return Num1; //Return The Greater Number
             }
             if (Num2.CompareTo(Num1) > 0 && Num2.CompareTo(Num3) > 0)
             {
@@ -22,7 +24,7 @@ namespace TestMaximumGenerics
             {
                 return Num3;
             }
-            return Num1;
+            return Num1; // Rturn While All Among Three are Equal
 
 
 
--- TestMaximumGenerics/Program.cs
+++ TestMaximumGenerics/Program.cs
@@ -4,23 +4,28 @@ namespace TestMaximumGenerics
 {
     class program
     {
+        // Main Method
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t******** WELCOME TO FIND MAXIMUM PROBLEM USING GENERICS ********");
 
-            int Choice;
+            int Choice; //Creating Variable to Store Input From User
             do
             {
                 Console.WriteLine("0.Exit");
                 Console.WriteLine("1.Find Maximum Among Three Integer");
                 Console.Write("Enter Your Choice : ");
-                Choice=Convert.ToInt32(Console.ReadLine());
+                Choice=Convert.ToInt32(Console.ReadLine()); // store iput From User & Convert Into Integer type
 
+                // Using Switch Case To Excute Prgram Input by User
                 switch (Choice)
                 {
                     case 1:
+
+                        // Creating Instance of Class FindMaxInt
                         FindMaxInt objFindMaxInt =new FindMaxInt();
 
+                        // printing Max Int No. AMong Three
                         Console.WriteLine("Maximum Integer Number = {0}", objFindMaxInt.FindMaxAmongInt(30, 20, 10));
 
                         Console.WriteLine("Maximum Integer Number = {0}", objFindMaxInt.FindMaxAmongInt(10, 30, 20));
@@ -31,6 +36,7 @@ namespace TestMaximumGenerics
                 }
             } while(Choice !=0);
 
+            //TO Hold OutPut Terminal
             Console.ReadKey();
         }
     }
