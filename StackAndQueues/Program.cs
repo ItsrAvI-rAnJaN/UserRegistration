5c0a5f7bd02f8700736c01626feddc1974dfc036
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-02 15:53:02 +0530

    Welcome Msg Stack&Queues

---
+++ StackAndQueues/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StackAndQueues
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t******* WELCOME TO STACK AND QUEUES PROGRAM ********");
+            Console.ReadKey();
+        }
+    }
+}
