f0aca0c5830aa1e3b52afb91d2cac7e909359ed6
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-07 10:59:02 +0530

    Workshop Questions

---
+++ harmonicno/HarmonicNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonicno
+{
+    internal class HarmonicNumber
+    {
+        public void PrintHarmonicNo()
+
+        {
+            Console.WriteLine("Enter the N to Find the Nth Value Harmonic Number");
+            int N = Convert.ToInt32(Console.ReadLine());
+            float HarmonicNo = 0;
+            if (N > 0)
+            {
+                for (float i = 1; i <= N; i++)
+                {
+                    HarmonicNo =HarmonicNo+ 1 / i;
+                }
+                Console.WriteLine("{0}th Harmonic Numberis : {1}", N, HarmonicNo);
+
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Positive No");
+            }
+        }
+    }
+}
---
+++ harmonicno/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace harmonicno
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            HarmonicNumber objHarmonic =new HarmonicNumber();
+            objHarmonic.PrintHarmonicNo();
+        }
+    }
+}
