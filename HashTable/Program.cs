7c1a616c6a0d660b021b67823d391aeb2b545245
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-11 23:21:00 +0530

    welcome msg to Hash Table

---
+++ HashTable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HashTable
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t*******WELCOME TO HASH MAP PROGRAM*******");
+            Console.ReadKey();
+        }
+    }
+}
