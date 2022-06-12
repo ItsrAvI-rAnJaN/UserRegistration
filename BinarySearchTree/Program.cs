b2741ae447a8732f03cfa3e6eb6bfee568a0d2c8
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-12 13:29:33 +0530

    Welcome Msg to Binay Tree

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t*******WELCOME TO BINARY SEARCH TREE PROGRAM*******");
+            Console.ReadKey();
+        }
+    }
+}
