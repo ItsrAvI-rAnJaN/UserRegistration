7f1bfb989f19d89635caf1be30376edb6dd00de2
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-12 17:12:41 +0530

    Welcome msg for UserReg. Program

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t******** WELCOME TO USER REGESTRATION PROGRAM ********");
+            Console.ReadKey();
+        }
+    }
+}
