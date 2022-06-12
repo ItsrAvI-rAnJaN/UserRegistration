a15e344a54bb7324000cf732664ecf61cf2bb4c5
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-12 17:29:48 +0530

    Add Validation for First Name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -7,6 +7,27 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t\t******** WELCOME TO USER REGESTRATION PROGRAM ********");
+            while (true)
+            {
+                Console.WriteLine("\n1. Please Enter Valid First Name");
+
+                Console.WriteLine("Enter 0 for Exit ");
+                Console.WriteLine("Please Enter Your Choice");
+
+                int Choice = Convert.ToInt32(Console.ReadLine());
+                switch (Choice)
+                {
+                    case 1:
+                        Validation objFirstName = new Validation();
+                        Console.WriteLine(objFirstName.Name("First"));
+                        break;
+                    case 0:
+                         return;
+                    default:
+                        Console.WriteLine("Please Enter Correct Choice");
+                        break ;
+                }
+            }
             Console.ReadKey();
         }
     }
--- UserRegistration/Validation.cs
+++ UserRegistration/Validation.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
     internal class Validation
     {
+        public string Name(string name)
+        {
+            //Regular Expression for Validation of First Name
+            string REGEX_FIRSTNAME = "^[A-Z][A-Za-z]{2,8}$";
+
+            Console.Write("Enter {0}Name for Validation : ",name);
+            string EnteredName = Console.ReadLine();
+
+            //if Name Enter By User is match with Expression  then Valid if not Then Inva;id 
+            return Regex.IsMatch(EnteredName, REGEX_FIRSTNAME) ? (name + " Name is Valid") : (name + " Name is Invalid");
+        }
     }
 }
