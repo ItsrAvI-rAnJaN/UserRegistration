1c3f0fbee338e507ba1f5fea8269586512985e60
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-20 00:54:43 +0530

    Welcome msg Mood Analyzer

---
+++ MoodAnalyzers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoodAnalyzers
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t******** WELCOME TO MOOD ANALYZERS PROGRAM ********");
+            Console.ReadKey();
+        }
+    }
+}
