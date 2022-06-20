a34701d7fe3628fbc448ad752b3ae13d4420b1d8
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-20 19:49:44 +0530

    add Exception Handling

---
+++ UserRegistration/CustomExecption.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class CustomExecption : Exception
+    {
+        public string message;
+
+        public enum ExceptionType
+        {
+            NULL_STRING, EMPTY_STRING, INVALID_STRING
+        }
+
+        public ExceptionType type;
+
+        public CustomExecption(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
+
--- UserRegistration/Validation.cs
+++ UserRegistration/Validation.cs
@@ -2,80 +2,179 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace UserRegistration
 {
-    internal class Validation
+    public class Validation
     {
         public string Name(string name)
         {
-            //Regular Expression for Validation of First Name
-            string REGEX_FIRSTNAME = "^[A-Z][A-Za-z]{2,8}$";
+            //Regular expression or validating name
+            string REGEX_FIRSTNAME = "^[A-Z][A-Za-z]{2,}$";
 
-            Console.Write("\nEnter {0} Name for Validation : ", name);
-            string EnteredName = Console.ReadLine();
+            Console.Write("Enter " + name + " Name for validation : ");
+            string enteredName = Console.ReadLine();
 
-            //if Name Enter By User is match with Expression  then Valid if not Then Inva;id 
-            return Regex.IsMatch(EnteredName, REGEX_FIRSTNAME) ? (name + " Name is Valid") : (name + " Name is Invalid");
+            try
+            {
+                if (enteredName.Equals(""))
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.EMPTY_STRING, "Name should not be empty");
+                }
+                if (enteredName.Equals(null))
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.NULL_STRING, "Name should not be ull");
+                }
+                if (Regex.IsMatch(enteredName, REGEX_FIRSTNAME))
+                {
+                    Console.WriteLine(name + " Name is valid");
+                    return enteredName;
+                }
+                else
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.INVALID_STRING, "Name should be valid");
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                return ex.Message;
+            }
         }
 
-        // Method for regular  Validation of  email
         public string Email(string email)
         {
-            string REGEX_EMAIL = "^([A-Za-z]){3,}([_.+-]{1}[0-9A-Za-z]{1,}){0,}@[a-z0-9]+[.](com|net){0,1}([.]((com)|([a-z]{2}){0,1})){0,1}$";
+            //regular expression for validating email
+            string REGEX_EMAIL = "^([A-Za-z]){3,}(([_.+-]?)([0-9A-Za-z]{1,}))*@[a-z0-9]+[.](com|net){0,1}([.]((com)|([a-z]{2}){0,1})){0,1}$";
 
-            Console.Write("Enter Email Name for validation : ");
-            string enteredEmail = Console.ReadLine();
-            return Regex.IsMatch(enteredEmail, REGEX_EMAIL) ? "Email is valid" : "Email is invalid";
+            //Console.Write("Enter Email Name for validation : ");
+            //string enteredEmail = Console.ReadLine();
+
+            try
+            {
+                if (email.Equals(""))
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.EMPTY_STRING, "Email should not be empty");
+                }
+                if (email.Equals(null))
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.NULL_STRING, "Email should not be ull");
+                }
+                if (Regex.IsMatch(email, REGEX_EMAIL))
+                {
+                    Console.WriteLine("Email is valid");
+                    return email;
+                }
+                else
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.INVALID_STRING, "Email should be valid");
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                return ex.Message;
+            }
         }
-        // method for Validation of Mobile Number
+
         public string MobileNumber()
         {
+            //Regular expression for checking mobile number, all mobile numbers starts with 6, 7, 8 or 9
             string REGEX_CELLNO = "^[+][1-9]{1,3}[ ][6-9]{1}[0-9]{9}$";
 
-            Console.Write("Enter Mobile Number Starting With Country Code : ");
+            Console.Write("Enter country code with Mobile number for validation (give a space between them, write '+' at the beginning) : ");
             string enteredMobileNumber = Console.ReadLine();
-            return Regex.IsMatch(enteredMobileNumber, REGEX_CELLNO) ? "Mobile Number is valid" : "Mobile Number is invalid";
 
+            try
+            {
+                if (enteredMobileNumber.Equals(""))
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.EMPTY_STRING, "Mobile number not be empty");
+                }
+                if (enteredMobileNumber.Equals(null))
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.NULL_STRING, "Mobile number not be ull");
+                }
+                if (Regex.IsMatch(enteredMobileNumber, REGEX_CELLNO))
+                {
+                    Console.WriteLine("Mobile number is valid");
+                    return enteredMobileNumber;
+                }
+                else
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.INVALID_STRING, "Mobile number be valid");
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                return ex.Message;
+            }
         }
-        // Method for Validation of Password 1
+
         public string Password1()
         {
-            string REGEX_PASSWORD = "^.{8,}$";
+            //Regular expression below will match any character except space i.e. " "
+            string REGEX_PASS = "^.{8,}$";
 
-            Console.Write("Enter Password for validation : ");
-            string EnteredPassword = Console.ReadLine();
-            return Regex.IsMatch(EnteredPassword, REGEX_PASSWORD) ? "Password is Valid" : "Password is Invalid";
+            Console.Write("Enter password for validation : ");
+            string enteredPassword = Console.ReadLine();
+            return Regex.IsMatch(enteredPassword, REGEX_PASS) ? "Password is valid" : "Password is invalid";
         }
+
         public string Password2()
         {
             //Regular expression to match previous condition + atleast one Upper Case
-            string REGEX_PASSWORD = "^(?=.*[A-Z]).{8,}$";
+            string REGEX_PASS = "^(?=.*[A-Z]).{8,}$";
 
             Console.Write("Enter password for validation : ");
-            string EnteredPassword = Console.ReadLine();
-            return Regex.IsMatch(EnteredPassword, REGEX_PASSWORD) ? "Password is valid" : "Password is Invalid";
+            string enteredPassword = Console.ReadLine();
+            return Regex.IsMatch(enteredPassword, REGEX_PASS) ? "Password is valid" : "Password is invalid";
         }
-        // Method For Password 3 having at least One Numeric Value
+
         public string Password3()
         {
-            string REGEX_PASSWORD = "^(?=.*[A-Z0-9]).{8,}$";
+            //Regular expression below will match all previous conditions + atleast one numeric value
+            string REGEX_PASS = "^(?=.*[A-Z0-9]).{8,}$";
 
             Console.Write("Enter password for validation : ");
-            string EnteredPassword = Console.ReadLine();
-            return Regex.IsMatch(EnteredPassword, REGEX_PASSWORD) ? "Password is Valid" : "Password is Invalid";
+            string enteredPassword = Console.ReadLine();
+            return Regex.IsMatch(enteredPassword, REGEX_PASS) ? "Password is valid" : "Password is invalid";
         }
-        //Method for Regular expression below will match previous conditions + exactly one special character
+
         public string Password4()
         {
-            string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^0-9a-zA-Z])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$";
+            //Regular expression below will match previous conditions + exactly one special character
+            string REGEX_PASS = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^0-9a-zA-Z])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$";
 
             Console.Write("Enter password for validation : ");
-            string EnteredPassword = Console.ReadLine();
-            return Regex.IsMatch(EnteredPassword, REGEX_PASSWORD) ? "Password is Valid" : "Password is Invalid";
+            string enteredPassword = Console.ReadLine();
+
+            try
+            {
+                if (enteredPassword.Equals(""))
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.EMPTY_STRING, "Password not be Empty");
+                }
+                if (enteredPassword.Equals(null))
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.NULL_STRING, "Password not be Null");
+                }
+                if (Regex.IsMatch(enteredPassword, REGEX_PASS))
+                {
+                    Console.WriteLine("Password is Valid");
+                    return enteredPassword;
+                }
+                else
+                {
+                    throw new CustomExecption(CustomExecption.ExceptionType.INVALID_STRING, "Password be Valid");
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                return ex.Message;
+            }
         }
+
         // Method to Test Valid & InValid email
         public void EmailsTest()
         {
